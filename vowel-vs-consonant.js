1b4368e38d48d65a25ee22bc2828563cc1bc8e0f
phiroun <man.phiroun@gmail.com>
2023-04-24 20:26:48 -0700

    updating file

--- vowel-vs-consonant.js
+++ vowel-vs-consonant.js
@@ -2,19 +2,26 @@
 //every other letter is considered a consonant.
 //define two variables that keep track of the number of consonants and vowels.
 //"hello"
-function vowelCount(str) {
-    let count = 0;
-    let vowels = "aeiou";
-    let consonants = "bcdfghjklmnpqrstvwxyz"
+// //"ukelele"
+// //"awesome"
+// //"onomonopia"
+// //"textbook "
 
-    for (let i = 0; i < str.legnth; i++){
-        if (vowels.indexOf(str[i].toLowerCase()) > -1){
-            count++;
-        }
-        return """ has" + count + vowels
+
+function countLetters(word) {
+
+    let consonantsCount = 0;
+    let vowelsCount = 0;
+ 
+    for (let i = 0; i < word.length; i++) {
+    
+      if (word[i] === 'a' || word[i] === 'e' || word[i] === 'i' || word[i] === 'o' || word[i] === 'u') {
+            vowelsCount++;
+      } else {
+        consonantsCount++;
+      }
     }
-}
-//"ukelele"
-//"awesome"
-//"onomonopia"
-//"textbook "
+  
+    console.log(word + ' has ' + consonantsCount + ' consonants and ' + vowelsCount + ' vowels');
+  }
+    countLetters('hello');
