19146ef7c63a2b7c410e2fc71cfd0d2b1c50ff11
phiroun <man.phiroun@gmail.com>
2023-04-24 20:55:39 -0700

    new files

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -2,3 +2,22 @@
 //for each multiple of 3, print "Fizz" instead of number.
 //for each multiple of 5, print "Buzz" instead of number.
 //for numbers that are multiple of both 3 and 5, print "FizzBuzz" instead of number.
+
+for (let i = 1; i <= 100; i++) {
+  // If the current number is a multiple of both 3 and 5, then print "FizzBuzz"
+  if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+  }
+  // If the current number is a multiple of 3, then print "Fizz"
+  else if (i % 3 === 0) {
+    console.log("Fizz");
+  }
+  // If the current number is a multiple of 5, then print "Buzz"
+  else if (i % 5 === 0) {
+    console.log("Buzz");
+  }
+  // If the current number is not a multiple of both 3 and 5, then print that current number.
+  else {
+    console.log(i);
+  }
+}
--- only-odd.js
+++ only-odd.js
@@ -1,2 +1,17 @@
-let nums = [2, 4, 6, 8, 11, 20, 15, 22];
-let outPut = arr.filter(x=>x % 2 === 0)
+//Using array to find odd nums
+function getOddNumbers(arr) {
+  const oddNumbers = [];
+
+  //Using loop to find each element in array
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] % 2 !== 0) {
+      oddNumbers.push(arr[i]);
+    }
+  }
+
+  return oddNumbers;
+}
+const oddNumbersArray = getOddNumbers([2, 4, 6, 8, 11, 20, 15, 22]);
+
+console.log(oddNumbersArray);
