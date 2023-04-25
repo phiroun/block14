45a5960bd2a97c5039731b33d616e0bf19d99bc4
phiroun <man.phiroun@gmail.com>
2023-04-24 17:25:17 -0700

    new update

---
+++ fizzbuzz.js
@@ -0,0 +1,4 @@
+//number 1-100
+//for each multiple of 3, print "Fizz" instead of number.
+//for each multiple of 5, print "Buzz" instead of number.
+//for numbers that are multiple of both 3 and 5, print "FizzBuzz" instead of number.
--- vowel-vs-consonant.js
+++ vowel-vs-consonant.js
@@ -2,7 +2,19 @@
 //every other letter is considered a consonant.
 //define two variables that keep track of the number of consonants and vowels.
 //"hello"
+function vowelCount(str) {
+    let count = 0;
+    let vowels = "aeiou";
+    let consonants = "bcdfghjklmnpqrstvwxyz"
+
+    for (let i = 0; i < str.legnth; i++){
+        if (vowels.indexOf(str[i].toLowerCase()) > -1){
+            count++;
+        }
+        return """ has" + count + vowels
+    }
+}
 //"ukelele"
 //"awesome"
 //"onomonopia"
-//"textbook"
+//"textbook "
